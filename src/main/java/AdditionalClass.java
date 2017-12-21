50d1f8575975fb06977c3623b9753fe651497d05
Dan Werner <werned@gmail.com>
2017-12-21 15:19:06 -0500

    Actually adding the work to the classes

--- src/main/java/AdditionalClass.java
+++ src/main/java/AdditionalClass.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class AdditionalClass {
+    private String petName;
+    private int petAge;
+
+    public AdditionalClass() {
+    }
+
+    public AdditionalClass(String petName, int petAge) {
+        this.petName = petName;
+        this.petAge = petAge;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public int getPetAge() {
+        return petAge;
+    }
+
+    public void setPetAge(int petAge) {
+        this.petAge = petAge;
+    }
+
+    @Override
+    public String toString() {
+
+        return "AdditionalClass{" +
+                "petName='" + petName + '\'' +
+                ", petAge=" + petAge +
+                '}';
+    }
 }
--- src/main/java/TestGitHub.java
+++ src/main/java/TestGitHub.java
@@ -1,5 +1,9 @@
 public class TestGitHub {
     public static void main(String[] args) {
         System.out.println("Using Git HUB! Can't commit and push with intellij. Updated message");
+        System.out.println("Adding some lines to see how merging works.")                ;
+
+        AdditionalClass ad = new AdditionalClass("Mackenzie", 2);
+        System.out.println(ad.toString());
     }
 }
