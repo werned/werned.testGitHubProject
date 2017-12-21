1bad54bf5e8e702500a1f3e25e3c05fe17c51817
Dan Werner <werned@gmail.com>
2017-12-21 12:12:15 -0500

    Now committing and pushing from intelliJ

--- src/main/java/TestGitHub.java
+++ src/main/java/TestGitHub.java
@@ -1,5 +1,5 @@
 public class TestGitHub {
     public static void main(String[] args) {
-        System.out.println("Using Git HUB!");
+        System.out.println("Using Git HUB! It pushed when I didn't use IntelliJ!");
     }
 }
